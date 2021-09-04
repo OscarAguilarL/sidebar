23f825ba2b9e4e284ac5425c8fbd1e0243549386
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-04 18:08:51 -0500

    Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './Menu.css';
+import logo from '../../assets/img/logo.png';
+import menu from '../../assets/img/menu.svg';
 
-export const Menu = () => {
-  return <div>menu works!</div>;
+export const Menu = ({ setIsOpen, isOpen }) => {
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`Menu ${isOpen && 'is-open'}`}>
+      <button className="menu-hamburguer" onClick={handleClick}>
+        <img src={menu} alt="" />
+      </button>
+
+      <div className="menu-logo">
+        <img src={logo} alt="" width="32" height="32" />
+        <h2 className="title">No te rindas</h2>
+      </div>
+    </div>
+  );
 };
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from '../Menu/Menu';
 
 import './Sidebar.css';
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div className="Sidebar is-open">
-      <Menu />
+    <div className={`Sidebar ${isOpen && 'is-open'}`}>
+      <Menu setIsOpen={setIsOpen} isOpen={isOpen} />
     </div>
   );
 };
