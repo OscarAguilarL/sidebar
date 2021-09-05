2deb44e6f893f02569294881725939176a33b015
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-09-05 11:10:57 -0500

    Add react routing and definde sidebar routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { Sidebar } from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
     <>
-      <Sidebar />
+      <Router>
+        <Sidebar />
+
+        <Switch>
+          <Route path="/">
+            <Redirect to="/recibidos" />
+          </Route>
+          <Route path="/recibidos" exact>
+            <Recibidos />
+          </Route>
+          <Route path="/destacados">
+            <Destacados />
+          </Route>
+          <Route path="/pospuestos">
+            <Pospuestos />
+          </Route>
+          <Route path="/enviados">
+            <Enviados />
+          </Route>
+          <Route path="/todos">
+            <Todos />
+          </Route>
+          <Route path="/papelera">
+            <Papelera />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
+
+const Recibidos = () => <h2>Recibidos</h2>;
+const Destacados = () => <h2>Destacados</h2>;
+const Pospuestos = () => <h2>Pospuestos</h2>;
+const Enviados = () => <h2>Enviados</h2>;
+const Todos = () => <h2>Todos</h2>;
+const Papelera = () => <h2>Papelera</h2>;
 
 export default App;
--- src/components/NavigationItem/NavigationItem.js
+++ src/components/NavigationItem/NavigationItem.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './NavigationItem.css';
 
 export const NavigationItem = ({ icon, title, selected }) => {
+  let slug = title.toLowerCase();
+  // if(title === 'Recibidos') {
+  //   slug = '';
+  // }
+
   return (
-    <div className={`navigationItem ${selected && 'is-selected'}`}>
+    <NavLink
+      className="navigationItem"
+      to={`/${slug}`}
+      activeClassName="is-selected"
+    >
       <button className="navigationItem-button">
         <i className={`icon-${icon}`} aria-hidden="true"></i>
       </button>
-      <p className={`navigationItem-title ${selected && 'is-selected'}`}>{title}</p>
-    </div>
+      <p className="navigationItem-title">{title}</p>
+    </NavLink>
   );
 };
